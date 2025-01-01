b02d2a14474d61bd49eb27e95f4bd461dae99fb6
Joe <yjoe2035@gmail.com>
2024-12-31 20:53:39 -0800

    Cleaned up code.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const container = document.querySelector(".container");
 const size = document.querySelector(".size");
 const reset = document.querySelector(".reset");
+const dark = document.querySelector(".dark");
 const display = document.querySelector(".display");
 let gridSize = 16;
+let opactiyValue = 1;
 
-
+grid(gridSize)
 function grid(gridSize) {
     console.log(gridSize)
     for (let i = 0; i < gridSize*gridSize; i++) {
         const createDiv = document.createElement("div");
         createDiv.classList.add("grid");
-        createDiv.style.width = 640/gridSize + "px";
-        createDiv.style.height = 640/gridSize + "px";
+        createDiv.style.width = 600/gridSize + "px";
+        createDiv.style.height = 600/gridSize + "px";
         createDiv.addEventListener("mouseover", () => {
             createDiv.classList.add("active");
         });
@@ -26,10 +28,20 @@ function gridReset() {
         container.removeChild(grid);
     });
     grid(gridSize);
+    opactiyValue = 1
 }
 
 size.addEventListener("click", () => {
     gridSize = Number(prompt("What would you like the grid size to be by? Default 16 x 16."));
     gridReset();
 })
-reset.addEventListener("click", gridReset());
+reset.addEventListener("click", gridReset);
+dark.addEventListener("click", () => {
+    const createDiv = document.querySelectorAll(".grid");
+    createDiv.forEach(grid => {
+        grid.addEventListener("mouseover", () => {
+            grid.style.opacity = opactiyValue*0.05;
+            opactiyValue++;
+        });
+    });
+})
